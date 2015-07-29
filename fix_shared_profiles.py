c0a08d44bd00025b0e7b996e5b29f5ebddc9ce5f
Ryan Manly <ryan.manly@gmail.com>
2015-07-29 16:49:32 -0500

    don't need profiles list anymore

--- fix_shared_profiles.py
+++ fix_shared_profiles.py
@@ -4,17 +4,19 @@ import argparse
 import plistlib
 from Foundation import NSUUID
 
-profiles = []
-
 def new_uuid():
     return NSUUID.UUID().UUIDString().lower().encode('ascii', 'ignore')
 
 def read_profile(profile):
     return plistlib.readPlist(profile)
 
-parser = argparse.ArgumentParser(description='Generate new uuids for shared configuration profiles.')
-parser.add_argument('file', help='one or more configuration profiles', nargs='+')
-args = parser.parse_args()
+def fix_top_level(profile):
+    profile
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Generate new uuids for shared configuration profiles.')
+    parser.add_argument('file', help='one or more configuration profiles', nargs='+')
+    args = parser.parse_args()
 
 #for profile in profiles:
 #    new_profile = read_profile(profile)
