b63016946c60e1553cabe4fc569cf036c8c9ab98
Ryan Manly <ryan.manly@gmail.com>
2015-07-29 17:58:11 -0500

    finish it up

--- fix_shared_profiles.py
+++ fix_shared_profiles.py
@@ -7,18 +7,44 @@ from Foundation import NSUUID
 def new_uuid():
     return NSUUID.UUID().UUIDString().lower().encode('ascii', 'ignore')
 
-def read_profile(profile):
-    return plistlib.readPlist(profile)
+
+def read_profile(filename):
+    return plistlib.readPlist(filename)
+
 
 def fix_top_level(profile):
-    profile
+    profile['PayloadUUID'] = new_uuid()
+    return profile
+
+
+def fix_content_level(content):
+    '''
+    fix_content_level(list) -> list
+
+    Return a modified list with new uuids for use in PayloadContent section
+    of a configuration profile.
+    '''
+    for payload in content:
+        payload['PayloadUUID'] = new_uuid()
+    return content
+
+
+def fix_profile(file):
+    profile = read_profile(file)
+    profile['PayloadContent'] = fix_content_level(profile['PayloadContent'])
+    new_profile = fix_top_level(profile)
+    write_new(new_profile, file)
+
+
+def write_new(profile, filename):
+    plistlib.writePlist(profile, filename)
+
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Generate new uuids for shared configuration profiles.')
     parser.add_argument('file', help='one or more configuration profiles', nargs='+')
     args = parser.parse_args()
 
-#for profile in profiles:
-#    new_profile = read_profile(profile)
-#    new_profile['PayloadUUID'] = new_uuid()
-#    
+    for file in args.file:
+       fix_profile(file) 
